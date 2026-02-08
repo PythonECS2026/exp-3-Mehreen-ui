7476f4142eab1d901790e1075e79f52886542f71
Mehreen <mehreenansari4954@gmail.com>
2026-02-08 21:52:18 +0530

    Add gross salary calculator functionality
    Implement gross salary calculation and display details.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 # AIM: Write a Python program to calculate the gross salary of an employee.
-# Coder:
-# Date:
+# Coder: mehreen ansari
+# Date: 08\02\26
 
-# Write your code here
+print("****GROSS SALARY CALCULATOR****")
+basic_salary=float(input("Enter the basic salary : "))
+da = basic_salary*0.70
+ta = basic_salary*0.30
+hra = basic_salary*0.10
+gross_salary = basic_salary + da + ta + hra
+
+print("\nSalary Details :")
+print(f"{'Basic Salary' :15}\t{basic_salary}")
+print(f"{'DA' :15}\t{da}")
+print(f"{'TA' :15}\t{ta}")
+print(f"{'HRA' :15}\t{hra}")
+print(f"{'Gross Salary' :15}\t{gross_salary}")
